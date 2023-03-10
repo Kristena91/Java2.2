e9422a1d817a6b5a962ea8192977fb7a17f07484
Kristina D <kristena91@mail.ru>
2023-03-10 20:26:22 +0300

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int amoutOnTheAccount = 100;
-        int replenishment = 300;
+        int replenishment = 1100;
         int bonus = 0;
         if (replenishment > 1000) {
             bonus = replenishment / 100;
